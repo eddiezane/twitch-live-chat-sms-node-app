3414f1c6424c1ff97b7303a507104fd337425dd9
Eddie Zaneski <eddiezane@gmail.com>
2016-03-10 15:48:35 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const socketIo = require('socket.io')
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/inbound/sms', (req, res) => {
+  let body = req.body.Body
+  io.sockets.emit('new message', body)
+  let twiml = `
+  <Response>
+    <Message>Thanks for texting!</Message>
+  </Response>
+  `
+  res.send(twiml)
+})
+
+io.on('connection', socket => {
+  socket.on('new message', message => {
+    console.log('got new message', message)
+    socket.broadcast.emit('new message', message)
+  })
+})
+
+server.listen(3000, () => console.log('server is started'))
---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const socket = io('/')
+
+const inputBox = document.getElementById('message-input')
+const messagesDiv = document.getElementById('messages')
+
+socket.on('new message', addNewMessage)
+
+inputBox.addEventListener('keypress', event => {
+  if (event.keyCode == 13) {
+    let message = event.target.value
+    socket.emit('new message', message)
+    addNewMessage(message)
+    event.target.value = ''
+  }
+})
+
+function addNewMessage(message) {
+  let p = document.createElement('p')
+  p.innerText = message
+  p.className = 'message'
+  messagesDiv.insertBefore(p, messagesDiv.children[0])
+}
